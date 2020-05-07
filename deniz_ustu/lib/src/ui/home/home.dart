a67c0f7dc4f89458cede9fe17d6cfd43a3cf60f4
Burak Karahan <burak.karahan@mail.ru>
2020-05-07 03:15:03 +0300

    Add home screen

--- deniz_ustu/lib/src/ui/home/home.dart
+++ deniz_ustu/lib/src/ui/home/home.dart
@@ -3,7 +3,6 @@ import 'package:getflutter/getflutter.dart';
 import 'package:control_pad/control_pad.dart';
 import 'package:video_player/video_player.dart';
 
-
 class HomeWidget extends StatefulWidget {
   @override
   _HomeWidgetState createState() => _HomeWidgetState();
@@ -27,27 +26,76 @@ class _HomeWidgetState extends State<HomeWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            color: const Color(0xFF212121),
-            padding: EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _controller.value.initialized
-                    ? AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                )
-                    : Container(),
-                Row(
+      body: Container(
+          color: const Color(0xFF212121),
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _controller.value.initialized
+                  ? AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      child: VideoPlayer(_controller),
+                    )
+                  : Container(),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      JoystickView(size: 150),
-                      PadButtonsView(size: 150),
-                    ])],
-            )
-        ),
-      );
+                    children: [
+                      Text(
+                        'Basınç:',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                          fontFamily: 'Roboto',
+                          letterSpacing: 0.5,
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        'İrtifa:',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                          fontFamily: 'Roboto',
+                          letterSpacing: 0.5,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ]),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Sıcaklık:',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                          fontFamily: 'Roboto',
+                          letterSpacing: 0.5,
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        'Motor Hızı:',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                          fontFamily: 'Roboto',
+                          letterSpacing: 0.5,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ])
+              ]),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    JoystickView(size: 150),
+                  ])
+            ],
+          )),
+    );
   }
 
   @override
