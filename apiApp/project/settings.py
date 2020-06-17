0a358be5b3a299d8b4e75666e041784aa08384fd
Burak Karahan <burak.karahan@mail.ru>
2020-06-17 19:54:43 +0300

    Add localhost to allowed host

--- apiApp/project/settings.py
+++ apiApp/project/settings.py
@@ -32,7 +32,7 @@ try:
 except KeyError:
     DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.40', '192.168.1.55']
+ALLOWED_HOSTS = ['192.168.1.40', '192.168.1.55', '127.0.0.1']
 
 
 # Application definition
